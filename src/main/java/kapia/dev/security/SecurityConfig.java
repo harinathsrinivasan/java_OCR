69e8c128d118df41746d9d1732841b3052d895b5
kapiaszczyk <ka.piaszczyk@gmail.com>
2024-02-01 16:33:41 +0100

    Add config class for Spring Security

---
+++ src/main/java/kapia/dev/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package kapia.dev.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.inMemoryAuthentication().withUser("user")
+                .password(passwordEncoder().encode("password")).roles("USER");
+    }
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
+                .csrf(csrf -> csrf.disable()).cors(corsSpec -> corsSpec.disable());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+
+
+}
