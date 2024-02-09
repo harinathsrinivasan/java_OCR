7bbc494a4ded08a089662061c9468195cd45625c
kapiaszczyk <ka.piaszczyk@gmail.com>
2024-02-09 17:41:02 +0100

    Remove unused imports

--- src/main/java/kapia/dev/DevApplication.java
+++ src/main/java/kapia/dev/DevApplication.java
@@ -1,17 +1,8 @@
 package kapia.dev;
 
-import kapia.dev.ocr.OCRController;
-import kapia.dev.ocr.OCRService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-
 
 
 @SpringBootApplication
