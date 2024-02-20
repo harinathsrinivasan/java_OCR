cde2e3affcd8bb05cb46cf488746860a5daa4255
kapiaszczyk <ka.piaszczyk@gmail.com>
2024-02-20 20:43:51 +0100

    Add validation for MultiPart file content type

--- src/main/java/kapia/dev/ocr/OCRController.java
+++ src/main/java/kapia/dev/ocr/OCRController.java
@@ -1,15 +1,15 @@
 package kapia.dev.ocr;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class OCRController {
@@ -17,11 +17,22 @@ public class OCRController {
     @Autowired
     OCRService ocrService;
 
-    @PostMapping("/getOCR")
+    @Value("${file.upload.content-type}")
+    private String contentTypes;
+
+    @PostMapping(value = "/getOCR", consumes = "multipart/form-data")
     @CrossOrigin(origins = "http://localhost:4200")
     public ResponseEntity<String> processImage(@RequestParam("image") MultipartFile image) {
 
+        List<String> allowedTypes = Arrays.asList(contentTypes.split(","));
+
         try {
+            if (image == null || image.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("File is empty");
+            }
+            if (!allowedTypes.contains(image.getContentType())) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("File is not an image");
+            }
             byte[] imageArr = image.getBytes();
             ResponseEntity<String> response = ocrService.processImage(imageArr);
             System.out.println(response.getBody());
