9b8a7f45a3825c8d31ccf94ab5186c07372b3d0a
kapiaszczyk <ka.piaszczyk@gmail.com>
2024-03-12 11:40:04 +0100

    Add logging
    Added logging for handling and resolving rate liming

--- src/main/java/kapia/dev/filters/RateLimitingFilter.java
+++ src/main/java/kapia/dev/filters/RateLimitingFilter.java
@@ -7,6 +7,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import kapia.dev.ratelimiting.RateLimitingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
@@ -19,6 +21,7 @@ import java.time.Duration;
 public class RateLimitingFilter extends OncePerRequestFilter {
 
     private final RateLimitingService rateLimitingService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(RateLimitingFilter.class);
 
     @Autowired
     public RateLimitingFilter(RateLimitingService rateLimitingService) {
@@ -29,6 +32,7 @@ public class RateLimitingFilter extends OncePerRequestFilter {
     public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
 
         if (hasAdminRole(request)) {
+            LOGGER.info("Resolved rate limiting for ROLE_ADMIN");
             chain.doFilter(request, response);
             return;
         }
@@ -49,6 +53,7 @@ public class RateLimitingFilter extends OncePerRequestFilter {
     private boolean handleRateLimitingWithApiKey(HttpServletRequest request, HttpServletResponse response) {
         String apiKey = request.getHeader("x-api-key");
         if (apiKey != null && !apiKey.isEmpty()) {
+            LOGGER.info("Handling rate limiting with API key");
             Bucket bucket = rateLimitingService.resolveBucketFromKey(apiKey);
             ConsumptionProbe probe = bucket.tryConsumeAndReturnRemaining(1);
             return handleRateLimitResult(probe, response);
@@ -59,6 +64,7 @@ public class RateLimitingFilter extends OncePerRequestFilter {
     private boolean handleRateLimitingWithIp(HttpServletRequest request, HttpServletResponse response) {
         String ip = request.getRemoteAddr();
         if (ip != null && !ip.isEmpty()) {
+            LOGGER.info("Handling rate limiting with IP");
             Bucket bucket = rateLimitingService.resolveBucketFromIp(ip);
             ConsumptionProbe probe = bucket.tryConsumeAndReturnRemaining(1);
             return handleRateLimitResult(probe, response);
@@ -68,8 +74,10 @@ public class RateLimitingFilter extends OncePerRequestFilter {
 
     private boolean handleRateLimitResult(ConsumptionProbe probe, HttpServletResponse response) {
         if (probe.isConsumed()) {
+            LOGGER.info("Token consumed");
             response.addHeader("X-Rate-Limit-Remaining", Long.toString(probe.getRemainingTokens()));
         } else {
+            LOGGER.info("Limit exceeded");
             response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
             response.addHeader("X-Rate-Limit-Retry-After-Seconds", Long.toString(Duration.ofNanos(probe.getNanosToWaitForRefill()).getSeconds()));
         }
