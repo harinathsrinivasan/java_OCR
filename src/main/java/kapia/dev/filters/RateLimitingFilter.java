c8e1066e073065f019a8c8f28491484afbc9c3b2
kapiaszczyk <ka.piaszczyk@gmail.com>
2024-03-12 10:56:16 +0100

    Refactor RateLimitingFilter for better readability
    Refactor the filter by splitting the doFilterInternal body
    into separate functions and remove SecurityResolverService
    since checking role can be handled by the filter.

--- src/main/java/kapia/dev/filters/RateLimitingFilter.java
+++ src/main/java/kapia/dev/filters/RateLimitingFilter.java
@@ -7,7 +7,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import kapia.dev.ratelimiting.RateLimitingService;
-import kapia.dev.security.SecurityResolverService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
@@ -20,58 +19,60 @@ import java.time.Duration;
 public class RateLimitingFilter extends OncePerRequestFilter {
 
     private final RateLimitingService rateLimitingService;
-    private final SecurityResolverService securityResolverService;
 
     @Autowired
-    public RateLimitingFilter(RateLimitingService rateLimitingService, SecurityResolverService securityResolverService) {
+    public RateLimitingFilter(RateLimitingService rateLimitingService) {
         this.rateLimitingService = rateLimitingService;
-        this.securityResolverService = securityResolverService;
     }
 
     @Override
     public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
 
-        if (securityResolverService.hasRole("ROLE_ADMIN")) {
-            System.out.println("Admin role detected, skipping rate limiting");
+        if (hasAdminRole(request)) {
             chain.doFilter(request, response);
             return;
         }
-
-        // Rate limit based on key
-        if (request.getHeader("x-api-key") != null && !request.getHeader("x-api-key").isEmpty()) {
-            System.out.println("API key detected, applying rate limit");
-            String apiKey = request.getHeader("x-api-key");
-            Bucket bucket = rateLimitingService.resolveBucketFromKey(apiKey);
-            ConsumptionProbe probe = bucket.tryConsumeAndReturnRemaining(1);
-            if (probe.isConsumed()) {
-                System.out.println("API key rate limit consumed");
-                System.out.println("Remaining tokens: " + probe.getRemainingTokens());
-                response.addHeader("X-Rate-Limit-Remaining", Long.toString(probe.getRemainingTokens()));
-                chain.doFilter(request, response);
-            } else {
-                System.out.println("API key rate limit exceeded");
-                response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
-                response.addHeader("X-Rate-Limit-Retry-After-Seconds", Long.toString(Duration.ofNanos(probe.getNanosToWaitForRefill()).getSeconds()));
-            }
+        if (handleRateLimitingWithApiKey(request, response)) {
+            return;
         }
-        // If no API-key, then check rate limit based on IP
-        else if (request.getRemoteAddr() != null && !request.getRemoteAddr().isEmpty()) {
-
-            Bucket bucket = rateLimitingService.resolveBucketFromIp(request.getRemoteAddr());
-            ConsumptionProbe probe = bucket.tryConsumeAndReturnRemaining(1);
-
-            if (probe.isConsumed()) {
-                System.out.println("IP rate limit consumed");
-                System.out.println("Remaining tokens: " + probe.getRemainingTokens());
-                response.addHeader("X-Rate-Limit-Remaining", Long.toString(probe.getRemainingTokens()));
-                chain.doFilter(request, response);
-            } else {
-                System.out.println("IP rate limit exceeded");
-                response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
-                response.addHeader("X-Rate-Limit-Retry-After-Seconds", Long.toString(Duration.ofNanos(probe.getNanosToWaitForRefill()).getSeconds()));
-            }
+        if (handleRateLimitingWithIp(request, response)) {
+            return;
         }
 
+        chain.doFilter(request, response);
     }
 
+    private boolean hasAdminRole(HttpServletRequest request) {
+        return request.isUserInRole("ROLE_ADMIN");
+    }
+
+    private boolean handleRateLimitingWithApiKey(HttpServletRequest request, HttpServletResponse response) {
+        String apiKey = request.getHeader("x-api-key");
+        if (apiKey != null && !apiKey.isEmpty()) {
+            Bucket bucket = rateLimitingService.resolveBucketFromKey(apiKey);
+            ConsumptionProbe probe = bucket.tryConsumeAndReturnRemaining(1);
+            return handleRateLimitResult(probe, response);
+        }
+        return false;
+    }
+
+    private boolean handleRateLimitingWithIp(HttpServletRequest request, HttpServletResponse response) {
+        String ip = request.getRemoteAddr();
+        if (ip != null && !ip.isEmpty()) {
+            Bucket bucket = rateLimitingService.resolveBucketFromIp(ip);
+            ConsumptionProbe probe = bucket.tryConsumeAndReturnRemaining(1);
+            return handleRateLimitResult(probe, response);
+        }
+        return false;
+    }
+
+    private boolean handleRateLimitResult(ConsumptionProbe probe, HttpServletResponse response) {
+        if (probe.isConsumed()) {
+            response.addHeader("X-Rate-Limit-Remaining", Long.toString(probe.getRemainingTokens()));
+        } else {
+            response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+            response.addHeader("X-Rate-Limit-Retry-After-Seconds", Long.toString(Duration.ofNanos(probe.getNanosToWaitForRefill()).getSeconds()));
+        }
+        return true;
+    }
 }
