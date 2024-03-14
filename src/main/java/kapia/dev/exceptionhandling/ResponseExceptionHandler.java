a4648c1058470b12ebba73ca96bf8c77f7acc05a
kapiaszczyk <41442206+kapiaszczyk@users.noreply.github.com>
2024-03-14 08:59:50 +0100

    Add logging statements for error handling and debugging

--- src/main/java/kapia/dev/exceptionhandling/ResponseExceptionHandler.java
+++ src/main/java/kapia/dev/exceptionhandling/ResponseExceptionHandler.java
@@ -1,6 +1,8 @@
 package kapia.dev.exceptionhandling;
 
 import net.sourceforge.tess4j.TesseractException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,18 +14,23 @@ import java.io.IOException;
 @ControllerAdvice
 public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(ResponseExceptionHandler.class);
+
     @ExceptionHandler(TesseractException.class)
     protected ResponseEntity<Object> handleProcessingException(TesseractException ex) {
+        LOGGER.error("Error processing the image", ex);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing the image");
     }
 
     @ExceptionHandler(IOException.class)
     protected ResponseEntity<Object> handleIOException(IOException ex) {
+        LOGGER.error("Error during image reading", ex);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error during image reading");
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     protected ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException ex) {
+        LOGGER.error("MultipartFile cannot be null", ex);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("MultipartFile cannot be null");
     }
 
--- src/main/java/kapia/dev/filters/FileValidationFilter.java
+++ src/main/java/kapia/dev/filters/FileValidationFilter.java
@@ -5,6 +5,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.Part;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,6 +14,8 @@ import java.io.IOException;
 
 @Component
 public class FileValidationFilter extends OncePerRequestFilter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileValidationFilter.class);
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -36,21 +40,25 @@ public class FileValidationFilter extends OncePerRequestFilter {
 
         String contentType = request.getContentType();
         if (contentType == null || !contentType.startsWith("multipart/form-data")) {
+            LOGGER.debug("Request does not have multipart/form-data content type");
             throw new ValidationException(HttpServletResponse.SC_BAD_REQUEST, "Request is not multipart/form-data");
         }
 
         Part image = request.getPart("image");
         if (image == null || image.getSize() == 0) {
+            LOGGER.debug("Image part was null or empty");
             throw new ValidationException(HttpServletResponse.SC_BAD_REQUEST, "Image is null or empty");
         }
 
         String fileName = image.getSubmittedFileName();
         if (!fileName.endsWith(".png") && !fileName.endsWith(".jpeg") && !fileName.endsWith(".jpg")) {
+            LOGGER.debug("Image did not have proper extension");
             throw new ValidationException(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE, "Image is not an image with proper extension");
         }
 
         String contentTypeOfImage = image.getContentType();
         if (!contentTypeOfImage.equals("image/png") && !contentTypeOfImage.equals("image/jpeg")) {
+            LOGGER.debug("Image was not of proper content type");
             throw new ValidationException(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE, "Image is not an image with proper content type");
         }
 
