e2e7b3031312897ec80db4c84134847980d7f6d3
kapiaszczyk <41442206+kapiaszczyk@users.noreply.github.com>
2024-03-21 08:50:53 +0100

    Move the specification of TESSDATA_PATH to application.properties
    Added a check that prevents the application
    to start when the path is empty.

--- src/main/java/kapia/dev/ocr/OCRService.java
+++ src/main/java/kapia/dev/ocr/OCRService.java
@@ -1,10 +1,12 @@
 package kapia.dev.ocr;
 
+import jakarta.annotation.PostConstruct;
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,9 +18,15 @@ import java.io.IOException;
 @Service
 public class OCRService {
 
-    final String TESSDATA_PATH = "tessdata";
+    @Value("${tessdata.path}")
+    private String TESSDATA_PATH;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(OCRService.class);
+
+    @PostConstruct
+    public void init() {
+        checkIfTesseractPathIsSet();
+    }
 
     public String processImage(MultipartFile image) throws IOException, TesseractException {
 
@@ -55,4 +63,12 @@ public class OCRService {
         return tesseract.doOCR(bufferedImage);
 
     }
+
+    private void checkIfTesseractPathIsSet() {
+        if (TESSDATA_PATH == null || TESSDATA_PATH.isEmpty()) {
+            LOGGER.error("Tessdata path is not set");
+            throw new IllegalArgumentException("Tessdata path is not set");
+        }
+    }
+
 }
