d5958f6c70babb3a842a652b91ca81cd7d49dc9e
kapiaszczyk <ka.piaszczyk@gmail.com>
2024-03-28 12:56:25 +0100

    Refactor redis connection code

--- src/main/java/com/kapia/redis/RedisConfig.java
+++ src/main/java/com/kapia/redis/RedisConfig.java
@@ -9,13 +9,14 @@ import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.codec.ByteArrayCodec;
 import io.lettuce.core.codec.RedisCodec;
 import io.lettuce.core.codec.StringCodec;
-import jakarta.annotation.PreDestroy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 import java.time.Duration;
 
@@ -24,6 +25,7 @@ import java.time.Duration;
 public class RedisConfig {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RedisConfig.class);
+
     @Value("${REDIS_HOST:localhost}")
     private String redisHost;
 
@@ -36,8 +38,10 @@ public class RedisConfig {
     @Value("${REDIS_KEY_PORT:6380}")
     private int redisKeyPort;
 
-    private RedisClient redisClient() {
-        LOGGER.info("Trying to connect to redis at {}:{}", redisHost, redisPort);
+    @Bean(destroyMethod = "shutdown")
+    @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public RedisClient redisClient(String redisHost, int redisPort) {
+        LOGGER.info("Creating a Redis client for host: {} and port: {}", redisHost, redisPort);
 
         return RedisClient.create(RedisURI.builder()
                 .withHost(redisHost)
@@ -45,40 +49,34 @@ public class RedisConfig {
                 .build());
     }
 
+    @Bean(destroyMethod = "close")
+    public StatefulRedisConnection<String, String> redisKeyConnection() {
+
+        LOGGER.info("Creating a Redis StatefulRedisConnection for host: {} and port: {}", redisKeyHost, redisKeyPort);
+        RedisClient redisClient = redisClient(redisKeyHost, redisKeyPort);
+
+        return redisClient.connect();
+    }
+
+    @Bean(destroyMethod = "close")
+    public StatefulRedisConnection<String,  byte[]> lettuceRedisConnection() {
+
+        LOGGER.info("Creating a Redis StatefulRedisConnection for host: {} and port: {}", redisHost, redisPort);
+        RedisClient redisClient = redisClient(redisHost, redisPort);
+
+        return redisClient
+                .connect(RedisCodec.of(StringCodec.UTF8, ByteArrayCodec.INSTANCE));
+    }
+
     @Bean
     public ProxyManager<String> lettuceProxyManager() {
-        RedisClient redisClient = redisClient();
-        StatefulRedisConnection<String, byte[]> redisConnection = redisClient
-                .connect(RedisCodec.of(StringCodec.UTF8, ByteArrayCodec.INSTANCE));
+
+        StatefulRedisConnection<String, byte[]> redisConnection = lettuceRedisConnection();
 
         return LettuceBasedProxyManager.builderFor(redisConnection)
                 .withExpirationStrategy(
                         ExpirationAfterWriteStrategy.basedOnTimeForRefillingBucketUpToMax(Duration.ofMinutes(1L)))
                 .build();
-    }
-
-    @PreDestroy
-    public void closeRedisClient() {
-        LOGGER.info("Closing redis client");
-        redisClient().shutdown();
-    }
-
-    public RedisClient redisKeyClient() {
-        LOGGER.info("Trying to connect to redis-keystore at {}:{}", redisKeyHost, redisKeyPort);
-
-        return RedisClient.create(RedisURI.builder()
-                .withHost(redisKeyHost)
-                .withPort(redisKeyPort)
-                .build());
-    }
-
-    @Bean
-    public StatefulRedisConnection<String, String> redisKeyConnection() {
-        RedisClient redisKeyClient = redisKeyClient();
-        StatefulRedisConnection<String, String> redisConnection = redisKeyClient.connect();
-
-        return redisConnection;
-
     }
 
 }
