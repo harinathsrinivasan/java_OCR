a1d5c5de5bfc1987b7a810c9dea31ffe22ee6f4d
kapiaszczyk <41442206+kapiaszczyk@users.noreply.github.com>
2024-04-02 09:55:53 +0200

    Add repository annotation

--- src/main/java/com/kapia/users/ApplicationUserRepository.java
+++ src/main/java/com/kapia/users/ApplicationUserRepository.java
@@ -1,9 +1,11 @@
 package com.kapia.users;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ApplicationUserRepository extends CrudRepository<ApplicationUser, Integer> {
 
     Optional<ApplicationUser> findByUsername(String username);
