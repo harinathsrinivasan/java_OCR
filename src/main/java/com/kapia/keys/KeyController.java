0e79227006d0216ccfbb886739f82ff32ccda7c2
kapiaszczyk <41442206+kapiaszczyk@users.noreply.github.com>
2024-04-05 13:14:37 +0200

    Add openAPI descriptions

--- src/main/java/com/kapia/keys/KeyController.java
+++ src/main/java/com/kapia/keys/KeyController.java
@@ -1,6 +1,12 @@
 package com.kapia.keys;
 
 import com.kapia.ratelimiting.PricingPlan;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/key")
+@SecurityRequirement(name = "basicAuth")
 public class KeyController {
 
     private final KeyService keyService;
@@ -19,7 +26,13 @@ public class KeyController {
     }
 
     @GetMapping
-    public String getKey(@RequestParam String plan) {
+    @Operation(summary = "Request a API key", description = "Endpoint that generates a new API key.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Key generated", content = @Content(mediaType = "text/plain")),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(mediaType = "text/plain")),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(mediaType = "text/plain")),
+    })
+    public String getKey(@Parameter(name = "plan", description = "Pricing plan for the key, can be either BASIC or PRO") @RequestParam String plan) {
         return keyService.generateKeyAndAddToRedis(PricingPlan.valueOf(plan));
     }
 
